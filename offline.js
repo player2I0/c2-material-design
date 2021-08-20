c10e255d4a8e3a4cc392f517562f8a016599e360
playerplayer210 <egorbubenov231@gmail.com>
2021-08-20 21:48:06 +0300

    added web version

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1629485227,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/shadow.png",
+		"images/rip-sheet0.png",
+		"images/button-sheet0.png",
+		"images/button-sheet1.png",
+		"images/button-sheet2.png",
+		"images/9patch.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
